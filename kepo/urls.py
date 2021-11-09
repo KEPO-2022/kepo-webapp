14fe2da79bb40a860daa403dac406dc3c6bd0b3b
Lawytel <adityadhiva88@gmail.com>
2021-11-09 11:17:54 +0700

    Added proper index template, css, and images. Internal html&css urls are fixed

--- kepo/urls.py
+++ kepo/urls.py
@@ -18,6 +18,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from kepo_app.views import IndexView
+
 urlpatterns = [
+    path('', IndexView.as_view(), name="index"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
